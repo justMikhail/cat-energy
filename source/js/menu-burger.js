fc2b17b303289934cbb4b20af7e5e112a2d2baa3
justMikhail <Mikhail.walle@gmail.com>
2020-12-24 05:42:12 +0500

    add JS for menu-burger

---
+++ source/js/menu-burger.js
@@ -0,0 +1,21 @@
+'use strict';
+
+(() => {
+  const mainNav = document.querySelector('.main-nav');
+  const mainNavBurger = document.querySelector('.header__burger');
+
+  mainNav.classList.remove('main-nav--nojs');
+  mainNav.setAttribute('aria-hidden', true); // доступность
+
+  console.log("работает");
+
+  const onMainNavBurgerClick = (evt) => {
+    evt.preventDefault();
+    mainNavBurger.classList.toggle('burger--closed');
+    mainNavBurger.classList.toggle('burger--opened');
+    mainNav.classList.toggle('main-nav--opened');
+  }
+
+  mainNavBurger.addEventListener('click', onMainNavBurgerClick);
+
+})();
