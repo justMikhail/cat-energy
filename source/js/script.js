28d81eb2a673f67a6ec1c34561d86733b55de1d1
justMikhail <Mikhail.walle@gmail.com>
2021-01-26 05:56:11 +0500

    промежуточный коммит

--- source/js/script.js
+++ source/js/script.js
@@ -7,8 +7,6 @@
   mainNav.classList.remove('main-nav--nojs');
   mainNav.setAttribute('aria-hidden', true);
 
-  console.log("работает");
-
   const onMainNavBurgerClick = (evt) => {
     evt.preventDefault();
     mainNavBurger.classList.toggle('burger--closed');
@@ -20,3 +18,47 @@
   mainNavBurger.addEventListener('click', onMainNavBurgerClick);
 
 })();
+
+(() => {
+  const switchSlider = document.querySelector('.switch-slider');
+
+  if (switchSlider) {
+    switchSlider.classList.add('switch-slider--js');
+    const second = switchSlider.querySelector('.switch-slider__item--after');
+    const secondImg = second.querySelector('img');
+    const input = switchSlider.querySelector('.switch-slider__input');
+    const pin = switchSlider.querySelector('.switch-slider__pin');
+    const beforeLabel = switchSlider.querySelector('.switch-slider__label--before');
+    const afterLabel = switchSlider.querySelector('.switch-slider__label--after');
+
+    const setStyles = (val) => {
+      pin.style.left = val;
+      second.style.width = val;
+    };
+
+    const setSecondImgPos = () => {
+      if (window.innerWidth < 768) {
+        beforeLabel.click();
+      } else {
+        const imgWidth = secondImg.clientWidth;
+        const switchSliderWidth = switchSlider.clientWidth;
+        afterImage.style.right = `${(elWidth - imgWidth) / 2}px`;
+      }
+    };
+
+    const inputHandler = () => setStyles('${input.value}%');
+
+    input.addEventListener('input', inputHandler);
+    input.addEventListener('change', inputHandler);
+    beforeLabel.addEventListener('click', () => setStyles('0'));
+    afterLabel.addEventListener('click', () => setStyles('100%'));
+    window.addEventListener('resize', setSecondImgPos);
+
+    setSecondImgPos();
+    if (window.innerWidth >= 768) {
+      setStyles('50%');
+    }
+  }
+})();
+
+console.log("it is works, Bro");
